6f3fe4633909af8f85c0b1da49f0a5cd67cc0b08
Johamin <gk0042@naver.com>
2026-03-07 18:41:15 +0900

    불필요한 파일 삭제

--- app/rag/check_dim.py
+++
@@ -1,8 +0,0 @@
-import chromadb
-
-client = chromadb.PersistentClient(path="./fr_chroma_db")
-collection = client.get_collection("fitness")   # embedding_function 제거
-
-res = collection.peek()
-peek_dim = len(res["embeddings"][0])
-print("현재 저장된 차원:", peek_dim)
